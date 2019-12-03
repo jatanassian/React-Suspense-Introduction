f14d09a3b42b9d6581d7dbe8c5a75cdc09d16144
Julien Atanassian <julien.atanassian@gmail.com>
2019-12-03 16:59:31 -0500

    fix app crashing by adding the missing parentheses

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 // Enable concurrent mode (set of features that stops the rendering from being blocked, it allows the rendering process to be interrupted so that you can improve the user experience)
-ReactDOM.createRoot(document.getElementById('root').render(<App />));
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
