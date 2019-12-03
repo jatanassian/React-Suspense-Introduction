9288c1b20029301fe056db2c67a6d0e6596438dd
Julien Atanassian <julien.atanassian@gmail.com>
2019-12-03 16:36:42 -0500

    create a wrapper function that handles the status of the fetch promises and a fetchdata function that call the two fetchers functions

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,46 @@
 import axios from 'axios';
 
+// Function that return an object
+export const fetchData = () => {
+  const userPromise = fetchUser();
+  const postsPromise = fetchPosts();
+  return {
+    user: wrapPromise(userPromise),
+    posts: wrapPromise(postsPromise)
+  }
+}
+
+// Wrapper that allows us to create the resource in the App component
+const wrapPromise = (promise) => {
+  // Set initial status
+  let status = 'pending';
+  // Store result
+  let result;
+  // Wait for promise
+  let suspender = promise.then(
+    res => {
+      status = 'success';
+      result = res;
+    }, 
+    err => {
+      status = 'error';
+      result = err;
+    }
+  )
+
+  return {
+    read() {
+      if (status === 'pending') {
+        throw suspender;
+      } else if (status === 'error') {
+        throw result;
+      } else if (status === 'success') {
+        return result;
+      }
+    }
+  }
+};
+
 const fetchUser = () => {
   console.log('Fetching User...');
   return axios.get('https://jsonplaceholder.typicode.com/users/1')
