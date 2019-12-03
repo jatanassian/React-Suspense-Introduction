5afa3bc56ab6118035c50f031e1ae6f5257ffe23
Julien Atanassian <julien.atanassian@gmail.com>
2019-12-03 16:16:34 -0500

    add comment about concurrent mode

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// Enable concurrent mode
+// Enable concurrent mode (set of features that stops the rendering from being blocked, it allows the rendering process to be interrupted so that you can improve the user experience)
 ReactDOM.createRoot(document.getElementById('root').render(<App />));
 
 // If you want your app to work offline and load faster, you can change
