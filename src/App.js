8f40b707b4bdfcb3e212a78ce861236f53106d0c
Julien Atanassian <julien.atanassian@gmail.com>
2019-12-03 16:46:09 -0500

    create profildetails component which reads and renders the info of the user

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ import { fetchData } from './Api';
 
 const resource = fetchData();
 
-function App() {
+const App = () => (
+  <div className='container my-5'>
+    <h1>Hello World</h1>
+  </div>
+);
+
+const ProfileDetails = () => {
+  const user = resource.user.read();
   return (
-    <div className='container my-5'>
-      <h1>Hello World</h1>
+    <div className='card card-body my-3'>
+      <h1 className='large text-primary'>{user.name}</h1>
+      <ul>
+        <li>Username: {user.name}</li>
+        <li>Email: {user.email}</li>
+        <li>City: {user.address.city}</li>
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
